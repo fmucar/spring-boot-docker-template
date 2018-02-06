866bd73d7b299dfe2cea076b61178a62c38888b8
Fatih Mehmet UCAR <fmucar@cooldatasoft.com>
2018-02-06 18:18:04 +0000

    initial import

---
+++ src/main/java/com/cooldatasoft/example/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.cooldatasoft.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hellow World!";
+    }
+}
